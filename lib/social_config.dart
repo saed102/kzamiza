f4f970dac83ca62ed631d9cb006b82717e6b1759
Mohamed Saed <saed8804@gmail.com>
2023-05-21 04:28:35 +0300

    first commit

---
+++ lib/social_config.dart
@@ -0,0 +1,8 @@
+class SocialConfig {
+  // var twitter_consumer_secret = "";
+  // var twitter_consumer_key = "";
+  //
+  var twitter_consumer_secret = "yE182I9rQqCzZIB7P1YfDZzc9VOJXIwCdsg2WaMejvR77Ff63G";
+  var twitter_consumer_key = "CFNfBPf0jD7asH3iYntEBQ7kQ";
+}
+
